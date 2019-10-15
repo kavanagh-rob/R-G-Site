96829fd6cc85dd6367b8402d3e2a1caa4dc97e42
Rob <kavanagh.m.rob@gmail.com>
2019-10-15 23:55:39 +0100

    click data

---
+++ js/clickdata.js
@@ -0,0 +1,48 @@
+;( function( window ) {
+    'use strict';
+
+    document.getElementById('rsvpMastHeadButton').addEventListener( 'click', function() {
+        postAnalytics('rsvpButtonMast');
+    });
+
+    document.getElementById('chatZoneButton').addEventListener( 'click', function() {
+        postAnalytics('chatzone');
+    });
+
+    document.getElementById('snapZoneButton').addEventListener( 'click', function() {
+        postAnalytics('snapzone');
+    });
+
+})( window );
+
+
+function postAnalytics(elementClicked) {
+    try {
+        var data = getAnalyticsData(elementClicked);
+        data.analyticsData.type = 'click-'+elementClicked;
+        $.ajax({
+            url: "https://37xim6z6ic.execute-api.eu-west-1.amazonaws.com/live/analytics",
+            data: JSON.stringify(data),
+            cache: false,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function (successData) {
+            },
+            error: function (errorData) {
+            }
+        });
+      } catch(err) {
+    }
+  }
+
+  function getAnalyticsData(elementClicked){
+    var data = {};
+    data.analyticsData = {};
+    data.analyticsData.elementClicked = elementClicked;
+
+    if(window.navigator) {
+        data.analyticsData.platform = data.analyticsData.platform ? data.analyticsData.platform : 'N/A';
+    }
+    return data;
+  }
--- js/pwa-install.js
+++ js/pwa-install.js
@@ -1,4 +1,3 @@
-
 'use strict'; 
 var deferredInstallPrompt = null;
 var installButton = document.getElementById('installButton');
@@ -8,6 +7,11 @@ installButton.addEventListener('click', installPWA);
 function installPWA(evt) {
   deferredInstallPrompt.prompt();
   evt.srcElement.setAttribute('hidden', true);
+
+  deferredInstallPrompt.userChoice.then(function(outcome) { 
+    postAnalytics(getInstallAnalyticsData(outcome), 'install')
+  }); 
+
 }
 
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
@@ -17,4 +21,33 @@ function saveBeforeInstallPromptEvent(evt) {
   deferredInstallPrompt = evt;
   installButton.removeAttribute('hidden');
   installedText.setAttribute('hidden', true);
+}
+
+
+function getInstallAnalyticsData(installPrompt) {
+  var analyticsData = {};
+  analyticsData.installPrompt = installPrompt;
+  analyticsData.installPrompt.platform = analyticsData.installPrompt.platform ? analyticsData.installPrompt.platform : 'N/A';
+  return analyticsData;
+}
+
+function postAnalytics(analyticsData, analyticsType) {
+  var data = {};
+  data.analyticsData = analyticsData;
+  data.analyticsData.type = analyticsType;
+  try {
+      $.ajax({
+          url: "https://37xim6z6ic.execute-api.eu-west-1.amazonaws.com/live/analytics",
+          data: JSON.stringify(data),
+          cache: false,
+          processData: false,
+          contentType: false,
+          type: 'POST',
+          success: function (successData) {
+          },
+          error: function (errorData) {
+          }
+      });
+    } catch(err) {
+  }
 }
